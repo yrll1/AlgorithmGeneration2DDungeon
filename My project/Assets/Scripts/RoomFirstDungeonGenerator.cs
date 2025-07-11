1e2fac6261e9d5d423851586454fffcccda76675
USER-20231003BQ\Administrator <2454382217@qq.com>
2025-07-12 01:01:02 +0800

    BPS和随机游走

--- My project/Assets/Scripts/RoomFirstDungeonGenerator.cs
+++ My project/Assets/Scripts/RoomFirstDungeonGenerator.cs
@@ -2,6 +2,7 @@ using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 public class RoomFirstDungeonGenerator : SimpleRandomWalkGenerator
@@ -25,10 +26,122 @@ public class RoomFirstDungeonGenerator : SimpleRandomWalkGenerator
         var roomList = ProceduralGenerationAlgorithms.BinarySpacePartitioning(new BoundsInt((Vector3Int)startPosition,new Vector3Int(dungeonWidth,dungeonHeight,0)),minRoomWidth,minRoomHeight);
 
         HashSet<Vector2Int> floor = new HashSet<Vector2Int>();//用于存储地板的位置。
-        floor = CreatesSimpleRooms(roomList);//根据 roomList 生成简单的房间，并将房间内的地板位置存储在 floor 集合中。
+        if(randomWalkRooms)
+        {
+            floor = CreateRoomsRandomly(roomList);
+        }
+        else
+        {
+        floor = CreatesSimpleRooms(roomList); //根据 roomList 生成简单的房间，并将房间内的地板位置存储在 floor 集合中。
+        }
+        
+       
+        List<Vector2Int> roomCenters = new List<Vector2Int>();
+        foreach (var room in roomList)//遍历房间列表
+        {
+            roomCenters.Add((Vector2Int)Vector3Int.RoundToInt(room.center));//将room.center通过RoundToInt方法四舍五入为正数
+                                                                            //然后通过显示类型转换获得X,Y
+        }
+        HashSet<Vector2Int> corridors = ConnectRooms(roomCenters);
+        floor.UnionWith(corridors);
         tilemapVisualizer.PaintFloorTiles(floor);//将地板位置绘制到地图上。
         WallGenerator.CreateWalls(floor, tilemapVisualizer);//根据地板位置生成墙壁。
 
+    }
+
+    private HashSet<Vector2Int> CreateRoomsRandomly(List<BoundsInt> roomList)
+    {
+        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            var roomBounds = roomList[i];
+            var roomCenter = new Vector2Int(Mathf.RoundToInt(roomBounds.center.x),Mathf.RoundToInt(roomBounds.center.y));
+            var roomFloor = RunRandowWalk(randomWalkParameters, roomCenter);
+            foreach (var position in roomFloor)
+            {
+                if(position.x>=(roomBounds.xMin + offset)&& position.x <= (roomBounds.xMax-offset)&&position.y>=(roomBounds.yMin-offset) && position.y<=(roomBounds.yMax-offset))
+                {
+                    floor.Add(position);
+                }
+            }
+        }
+        return floor;
+    }
+
+    private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCenters)//：将 roomCenters 列表中的所有房间中心点用走廊连接起来，最终返回一个包含所有走廊位置的 HashSet<Vector2Int>。
+    {
+       HashSet<Vector2Int> corridors = new HashSet<Vector2Int>();// 用于存储所有走廊的位置。
+        var currentRoomCenter = roomCenters[UnityEngine.Random.Range(0, roomCenters.Count)];//随机选择一个房间中心点作为起始点 currentRoomCenter，
+        roomCenters.Remove(currentRoomCenter); //并从 roomCenters 列表中移除该点。
+        while (roomCenters.Count > 0)
+        {
+             Vector2Int closet = FindClosesPointTo(currentRoomCenter, roomCenters);//调用 FindClosesPointTo 方法找到距离 currentRoomCenter 最近的点 closet。
+            roomCenters.Remove(closet);//从 roomCenters 列表中移除 closet。
+            HashSet<Vector2Int> newCorridor = CreateCorridor(currentRoomCenter,closet);//调用 CreateCorridor 方法创建连接 currentRoomCenter 和 closet 的走廊 newCorridor。
+            currentRoomCenter = closet;//更新 currentRoomCenter 为 closet。
+            corridors.UnionWith(newCorridor);// newCorridor 中的位置合并到 corridors 中。
+        }
+       return corridors;
+    }
+    //创建连接两个房间中心点的走廊，返回一个包含走廊位置的 HashSet<Vector2Int>。
+    private HashSet<Vector2Int> CreateCorridor(Vector2Int currentRoomCenter, Vector2Int destination)
+    {
+       HashSet<Vector2Int> corridor = new HashSet<Vector2Int>();//初始化一个 HashSet<Vector2Int> 类型的 corridor 用于存储走廊的位置。
+        var position = currentRoomCenter;//将起始点 currentRoomCenter 添加到 corridor 中。
+        corridor.Add(position);
+        //第一个 while 循环：
+        //如果目标点的 y 坐标大于当前点的 y 坐标，当前点向上移动一格（Vector2Int.up）。
+        //如果目标点的 y 坐标小于当前点的 y 坐标，当前点向下移动一格（Vector2Int.down）。
+        //将移动后的点添加到 corridor 中。
+    while (position.y!= destination.y)
+        {
+            if (destination.y>position.y)
+            {
+                position += Vector2Int.up;
+            }
+            else if (destination.y<position.y)
+            {
+                position += Vector2Int.down;
+            }
+            corridor.Add(position);
+        }
+        //第二个 while 循环：
+       // 如果目标点的 x 坐标大于当前点的 x 坐标，当前点向右移动一格（Vector2Int.right）。
+     //如果目标点的 x 坐标小于当前点的 x 坐标，当前点向左移动一格（Vector2Int.left）。
+    //将移动后的点添加到 corridor 中。
+        while (position.x!=destination.x)
+
+        {
+            if (destination.x>position.x)
+            {
+                position += Vector2Int.right;
+            }
+            else if (destination.x < position.x)
+            {
+                position += Vector2Int.left;
+            }
+            corridor.Add(position);
+        }
+        return corridor;
+    }
+
+    private Vector2Int FindClosesPointTo(Vector2Int currentRoomCenter, List<Vector2Int> roomCenters)
+    {
+       // 在 roomCenters 列表中找到距离 currentRoomCenter 最近的点，返回该点的 Vector2Int 坐标。
+        Vector2Int closest = Vector2Int.zero;//初始化 closest 为 Vector2Int.zero，
+        float distance = float.MaxValue;//distance 为 float.MaxValue。
+        foreach (var position in roomCenters)// roomCenters 列表中的每个点：
+        {//计算当前点与 currentRoomCenter 之间的距离 currentDistance。
+          // 如果 currentDistance 小于 distance，更新 distance 为 currentDistance，并将 closest 更新为当前点。
+            float currentDistance = Vector2.Distance(position,currentRoomCenter);
+            if (currentDistance < distance)
+            {//如果 currentDistance 小于 distance，更新 distance 为 currentDistance，并将 closest 更新为当前点。
+                distance = currentDistance;
+                closest = position;
+             
+            }
+        }
+        return closest;
     }
 
     private HashSet<Vector2Int> CreatesSimpleRooms(List<BoundsInt> roomList)
